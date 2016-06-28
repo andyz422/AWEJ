dbb6f5c4b8c4bbbf5e2dc943a8e508884d9a383a
Andy Zhu <andyz422@lapras.cs.uchicago.edu>
2016-06-28 17:00:13 -0500

    hi

---
+++ A/A/A/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  A
+//
+//  Created by Andy Zhu on 6/28/16.
+//  Copyright © 2016 Andy Zhu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
