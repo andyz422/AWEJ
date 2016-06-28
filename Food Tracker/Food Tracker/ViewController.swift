ca0dee7598c55be59c7ba1a383eebe73d695cb2a
Willypm <Willy81294@gmail.com>
2016-06-28 17:09:17 -0500

    Add files via upload

---
+++ Food Tracker/Food Tracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Food Tracker
+//
+//  Created by William Potebnya-Meyer on 6/28/16.
+//  Copyright © 2016 William Potebnya-Meyer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
