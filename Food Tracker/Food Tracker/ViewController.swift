8515a6801686fd0d9862491653174e38ca9ec24f
William Potebnya-Meyer <williampm@vaporeon.cs.uchicago.edu>
2016-07-11 18:01:41 -0500

    hi

--- Food Tracker/Food Tracker/ViewController.swift
+++ Food Tracker/Food Tracker/ViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: Properties
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +25,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: Actions
+    @IBAction func setDefaultLabelText(sender: UIButton) {
+        mealNameLabel.text = "Default Text"
+    }
 
 }
-
