624f0edb48f1cd3d438629016403237828dcf1d6
Andy Zhu <andyz422@jolteon.cs.uchicago.edu>
2016-07-11 17:35:25 -0500

    hi

---
+++ A/Food TrackerTests/Food_TrackerTests.swift
@@ -0,0 +1,36 @@
+//
+//  Food_TrackerTests.swift
+//  Food TrackerTests
+//
+//  Created by William Potebnya-Meyer on 6/28/16.
+//  Copyright © 2016 William Potebnya-Meyer. All rights reserved.
+//
+
+import XCTest
+@testable import Food_Tracker
+
+class Food_TrackerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
