93cc844e39bac78ef844ce04cf770596332946b1
Andy Zhu <andyz422@ninetales.cs.uchicago.edu>
2016-07-17 16:29:21 -0500

    hi

---
+++ FoodTracker/FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Andy Zhu on 7/17/16.
+//  Copyright © 2016 Andy Zhu. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
