ff4843af856182ecce4edaa0eb5dae891e4a0d10
Andy Zhu <andyz422@arbok.cs.uchicago.edu>
2016-08-01 16:36:07 -0500

    hi

--- Maze/Maze/ViewController.h
+++ Maze/Maze/ViewController.h
@@ -8,6 +8,8 @@
 
 #import <UIKit/UIKit.h>
 #import <QuartzCore/CAAnimation.h>
+#import <CoreMotion/CoreMotion.h>
+#define kUpdateInterval (1.0f / 60.0f)
 
 @interface ViewController : UIViewController
 
@@ -18,6 +20,17 @@
 @property (strong, nonatomic) IBOutlet UIImageView *exit;
 
 @property (strong, nonatomic) IBOutletCollection(UIImageView) NSArray *wall;
+
+@property (assign, nonatomic) CGPoint currentPoint;
+@property (assign, nonatomic) CGPoint previousPoint;
+@property (assign, nonatomic) CGFloat pacmanXVelocity;
+@property (assign, nonatomic) CGFloat pacmanYVelocity;
+@property (assign, nonatomic) CGFloat angle;
+@property (assign, nonatomic) CMAcceleration acceleration;
+@property (strong, nonatomic) CMMotionManager *motionManager;
+@property (strong, nonatomic) NSOperationQueue *queue;
+@property (strong, nonatomic) NSDate *lastUpdateTime;
+
 
 @end
 
