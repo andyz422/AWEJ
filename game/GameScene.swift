1501b7d99abec35fcc9df98fd6c38914416ea61f
Andy Zhu <andyz422@spearow.cs.uchicago.edu>
2016-08-06 15:54:12 -0500

    hi

--- game/GameScene.swift
+++ game/GameScene.swift
@@ -10,39 +10,45 @@ import SpriteKit
 
 class GameScene: SKScene {
     
+    let button = SKSpriteNode(imageNamed:"button")
+    let diglett = SKSpriteNode(imageNamed:"diglett")
+    
     override func didMoveToView(view: SKView) {
         
         self.backgroundColor = SKColor.blackColor()
         self.anchorPoint = CGPointMake(0.5, 0.5)
         
-        let button = SKSpriteNode(imageNamed: "button")
-        let background1 = SKSpriteNode(imageNamed:"background1")
-
+        self.addChild(button)
+        button.position = CGPointMake(200, 800)
+        
+        self.addChild(diglett)
+        diglett.position = CGPointMake(100, 200)
         
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        for touch in (touches) {
+        
+        for touch in touches {
             let location = touch.locationInNode(self)
         }
     }
     
-    /*override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        for touch in (touches as! Set<UITouch>) {
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        for touch in (touches ) {
             let location = touch.locationInNode(self)
             
             let v = CGVector(dx: location.x - button.position.x, dy: location.y - button.position.y)
             let angle = atan2(v.dy, v.dx)
             
             let deg = angle * CGFloat(180 / M_PI)
-            print(deg + 180)
+            print(deg + 180, terminator: "")
             
             let length:CGFloat = button.frame.size.height / 2
             
-            let xDist:CGFloat = sin(angle) * length
-            let yDist:CGFloat = cos(angle) * length
+            let xDist:CGFloat = sin(angle - 1.57079633) * length
+            let yDist:CGFloat = cos(angle - 1.57079633) * length
             
-            button.position = CGPointMake(button.position.x - xDist, button.position.y - yDist)
+            button.position = CGPointMake(button.position.x - xDist, button.position.y + yDist)
         }
-    }*/
+    }
 }
--- game/GameViewController.swift
+++ game/GameViewController.swift
@@ -9,24 +9,42 @@
 import UIKit
 import SpriteKit
 
-
+extension SKNode {
+    class func unarchiveFromFile(file: String) -> SKNode? {
+        if let path = NSBundle.mainBundle().pathForResource(file, ofType: "sks") {
+            let sceneData = try! NSData(contentsOfFile: path, options: .DataReadingMappedIfSafe)
+            let archiver = NSKeyedUnarchiver(forReadingWithData: sceneData)
+            
+            archiver.setClass(self.classForKeyedUnarchiver(), forClassName: "SKScene")
+            let scene = archiver.decodeObjectForKey(NSKeyedArchiveRootObjectKey) as! GameScene
+            archiver.finishDecoding()
+            return scene
+        } else {
+            return nil
+            
+        }
+    }
+}
 class GameViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let scene = GameScene(fileNamed: "GameScene")
-        let skView = self.view as! SKView
-        skView.showsFPS = true
-        skView.showsNodeCount = true
-        skView.ignoresSiblingOrder = true
-        scene!.scaleMode = .ResizeFill
-        skView.presentScene(scene)
+        if let scene = GameScene.unarchiveFromFile("GameScene") as? GameScene {
+            let skView = self.view as! SKView
+            skView.showsFPS = true
+            skView.showsNodeCount = true
+            
+            skView.ignoresSiblingOrder = true
+            
+            scene.scaleMode = .AspectFill
+            scene.size = skView.bounds.size
+            skView.presentScene(scene)
+        }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
 }
