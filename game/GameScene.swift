28ae64bd77eb8ac8a6958676ed935cfc2dae8117
Andy Zhu <andyz422@vaporeon.cs.uchicago.edu>
2016-08-07 20:05:27 -0500

    hi

--- game/GameScene.swift
+++ game/GameScene.swift
@@ -9,85 +9,89 @@
 import SpriteKit
 
 class GameScene: SKScene {
-    
-    let button = SKSpriteNode(imageNamed:"button")
-    let diglett = SKSpriteNode(imageNamed:"diglett")
-    let charmander = SKSpriteNode(imageNamed:"charmander")
-    let laser = SKSpriteNode(imageNamed:"laser")
-    let base = CGRect(x: -200, y: -840, width: 100, height: 100)
+    var button = SKSpriteNode(imageNamed:"button")
+    var diglett = SKSpriteNode(imageNamed:"diglett")
+    var charmander = SKSpriteNode(imageNamed:"charmander")
+    var laser = SKSpriteNode(imageNamed:"laser")
+    var base = CGRect(x: 0, y: 0, width: 0, height: 0)
     
     var stickActive:Bool = false
     
-    override func didMoveToView(view: SKView) {
-        
-        self.anchorPoint = CGPointMake(0.5, 0.5)
+    var diglett_inaction = false
 
-        self.addChild(button)
-        button.position = CGPointMake(-200, -840)
-        button.size.width = 100
-        button.size.height = 100
-        button.zPosition = 10
+    override func didMoveToView(view: SKView) {
+        button = self.childNodeWithName("button") as! SKSpriteNode
+        diglett = self.childNodeWithName("diglett") as! SKSpriteNode
+        charmander = self.childNodeWithName("charmander") as! SKSpriteNode
+        laser = self.childNodeWithName("laser") as! SKSpriteNode
         
-        self.addChild(diglett)
-        diglett.position = CGPointMake(0, -600)
-        diglett.size.height = 100
-        diglett.size.width = 100
         button.zPosition = 10
-        
-        self.addChild(charmander)
-        charmander.position = CGPointMake(0, 800)
-        charmander.size.width = 100
-        charmander.size.height = 100
+        diglett.zPosition = 10
         charmander.zPosition = 10
-        
-        self.addChild(laser)
-        laser.position = CGPointMake(0, 400)
-        laser.size.width = 50
-        laser.size.height = 100
         laser.zPosition = 10
         
+        base = button.frame
+        
+        let charmander_move_right = SKAction.moveByX(800, y: 0, duration: 3.0)
+        let charmander_move_left = SKAction.moveByX(-800, y: 0, duration: 3.0)
+        let charmander_sequence = SKAction.sequence([charmander_move_right, charmander_move_left])
+        let repeat_charmander = SKAction.repeatActionForever(charmander_sequence)
+        
+        charmander.runAction(repeat_charmander)
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
         for touch in touches {
             let location = touch.locationInNode(self)
-            
-            if (CGRectContainsPoint(base, location)) {
-                stickActive = true
-            } else {
-                stickActive = false
-            }
+            stickActive = CGRectContainsPoint(base, location)
         }
     }
     
     override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
         
-        for touch in (touches as! Set<UITouch>) {
+        
+        for touch in touches {
             let location = touch.locationInNode(self)
+            if (diglett_inaction) {
+                diglett.removeActionForKey("aKey")
+            }
             
             if (stickActive) {
-                let v = CGVector(dx: location.x - base.origin.x, dy: location.y - base.origin.y)
+                let v = CGVector(dx: location.x - base.midX, dy: location.y - base.midY)
                 let angle = atan2(v.dy, v.dx)
             
                 let length:CGFloat = 40
             
-                let xDist:CGFloat = sin(angle - 1.57079633) * length
-                let yDist:CGFloat = cos(angle - 1.57079633) * length
+                var xDist:CGFloat = sin(angle - 1.57079633) * length
+                var yDist:CGFloat = cos(angle - 1.57079633) * length
                 
                 if (CGRectContainsPoint(base, location)) {
+                    xDist = xDist / 2
+                    yDist = yDist / 2
+
                     button.position = location
                 
                 } else {
-                    button.position = CGPointMake(base.origin.x - xDist, base.origin.y + yDist)
+                    button.position = CGPointMake(base.midX - xDist, base.midY + yDist)
                 }
-                diglett.zRotation = angle - 1.57079633
+                
+                let action_move = SKAction.moveBy(CGVectorMake(-xDist, yDist), duration: 0.05)
+                let repeat_action = SKAction.repeatActionForever(action_move)
+                
+                diglett.runAction(repeat_action, withKey: "aKey")
+                diglett_inaction = true
             }
         }
     }
     
     override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+        if (diglett_inaction) {
+            diglett.removeActionForKey("aKey")
+        }
         if (stickActive == true) {
-            let move:SKAction = SKAction.moveTo(base.origin, duration: 0.2)
+            let move:SKAction = SKAction.moveTo(CGPoint(x: base.midX, y: base.midY), duration: 0.2)
             move.timingMode = .EaseOut
             
             button.runAction(move)
