e792ef02d9a853742c100b8ec72d1c2efb7e2df1
Andy Zhu <andyz422@zubat.cs.uchicago.edu>
2016-08-26 16:56:54 -0500

    hi

---
+++ game/MountainScene.swift
@@ -0,0 +1,14 @@
+//
+//  MountainScene.swift
+//  game
+//
+//  Created by Andy Zhu on 8/26/16.
+//  Copyright © 2016 DAW. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class MountainScene: SKScene {
+
+}
---
+++ game/Town2Scene.swift
@@ -0,0 +1,14 @@
+//
+//  Town2Scene.swift
+//  game
+//
+//  Created by Andy Zhu on 8/26/16.
+//  Copyright © 2016 DAW. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class Town2Scene: SKScene, SKPhysicsContactDelegate {
+
+}
